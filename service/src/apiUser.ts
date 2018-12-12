7a8adb2edf20748bd5e86467df7701e50c7b0940
Deployer <backup380@example.com>
2018-12-12 09:23:41 +0000

    Updates

--- service/src/apiUser.ts
+++ service/src/apiUser.ts
@@ -1,69 +1,55 @@
 import * as express from "express";
-import * as uuidv4 from "uuid/v4";
-import { IApiMessage, IServerResponse, MessageType } from "./shared/types";
 import { User } from "./entity/User";
 import { database } from "./sqliteConnection";
-import { sendResponse } from "./ApiResponse";
+
 
 
 const router = express.Router();
+
+// About page route.
+router.get('/about', function (req, res) {
+    res.send('About this wiki');
+})
+
+//req.get(bearer)
 
 router.post("/login", async function (request, response) {
     if (request.body.username && request.body.password) {
         try {
             const user: User = await database.loadUserByUsername(request.body.username);
             if (user.password === database.hash(request.body.password)) {
-                sendResponse(response, 200, { messages: [{ name: "api.success.user.login", type: MessageType.success }], payload: { token: uuidv4() } });
+                response.json({ success: { message: "api.success.user.login" },
+                                token: "TOKEN-HARDCODETOKEN"}); //Token mit uuid/v5
             } else {
-                sendResponse(response, 400, { messages: [{ name: "api.error.user.login.missing-username", type: MessageType.error }] });
+                response.json({ error: { message: "api.error.user.login.missing-username" } });
             }
         } catch (error) {
-            sendResponse(response, 404, { messages: [{ name: "api.error.user.login.user-not-found", type: MessageType.error, args: { "username": request.body.username } }]});
+            response.json({ error: { message: "api.error.user.login.user-not-found" } });
         }
     } else {
-        let errormessages: IApiMessage[] = [];
-        if (!request.body.username) {
-            errormessages.push({ name: "api.error.user.login.missing-data.username", type: MessageType.error });
-        }
-        if (!request.body.password) {
-            errormessages.push({ name: "api.error.user.login.missing-data.password", type: MessageType.error });
-        }
-        sendResponse(response, 400, { messages: errormessages });
+        response.json({ error: { message: "api.error.user.login.missing-data" } });
     }
 });
+
+//response.status(201).json({ message: { name: "api.success.asdf", args: { id: 1 } }, payload: { token: "asfiunwelrifujsöfjkl" } });
 
 router.post("/register", async function (request, response) {
     if (request.body.firstName && request.body.lastName && request.body.username && request.body.email && request.body.password) {
         try {
-            if (await database.loadUserByUsername(request.body.username)) {
-                sendResponse(response, 400, { messages: [{ name: "api.error.user.register.username-already-exists", type: MessageType.error, args: { "username": request.body.username } }]});
-            } else {
+            if(database.loadUserByEmail(request.body.email) || database.loadUserByUsername(request.body.username)){
+                response.json({ success: { message: "api.error.user.register.userdata-already-exists" }});
+            }else{
                 database.createUser(request.body.firstName, request.body.lastName, request.body.username, request.body.email, request.body.password);
-                sendResponse(response, 200, { messages: [{ name: "api.success.user.register", type: MessageType.success }]});
+                response.json({ success: { message: "api.success.register" }});
             }
         } catch (error) {
-            let errorstring: string = error.toString();
-            sendResponse(response, 400, { messages: [{ name: "api.error.user.register.other", type: MessageType.error, args: { "error": errorstring } }]});
+            response.json({ error: { message: "api.error.user.register.other" } });
         }
     } else {
-        let errormessages: IApiMessage[] = [];
-        if (!request.body.firstName) {
-            errormessages.push({ name: "api.error.user.missing-data.firstName", type: MessageType.error });
-        }
-        if (!request.body.lastName) {
-            errormessages.push({ name: "api.error.user.missing-data.lastName", type: MessageType.error });
-        }
-        if (!request.body.username) {
-            errormessages.push({ name: "api.error.user.missing-data.username", type: MessageType.error });
-        }
-        if (!request.body.email) {
-            errormessages.push({ name: "api.error.user.missing-data.email", type: MessageType.error });
-        }
-        if (!request.body.password) {
-            errormessages.push({ name: "api.error.user.missing-data.password", type: MessageType.error });
-        }
-        sendResponse(response, 400, { messages: errormessages });
+        response.json({ error: { message: "api.error.missing-data" }});
+        // TODO: genauere errormessages
     }
 });
+
 
 export default router;
