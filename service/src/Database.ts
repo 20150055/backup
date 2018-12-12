416305ccf256253ff672782e27c52977fb108b72
Deployer <backup380@example.com>
2018-12-12 11:25:16 +0000

    Updates

--- service/src/Database.ts
+++ service/src/Database.ts
@@ -78,9 +78,9 @@ export class Database {
         return user;
     }
 
-    async countUsers(){
-        const count: number = await this.connection.manager.getRepository(User).count();
-        return count;
+    async loadUserByEmail(searchEmail: string){
+        const user: User = await this.connection.manager.getRepository(User).findOne({username: searchEmail});
+        return user;
     }
     
     hash(word: string) :string {
