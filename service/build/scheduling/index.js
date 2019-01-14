f55cf6f60466235be83fdb1337356ae559ca51c1
Deployer <backup380@example.com>
2019-01-14 18:37:00 +0000

    Updates

--- service/build/scheduling/index.js
+++ service/build/scheduling/index.js
@@ -1,4 +1,6 @@
 //import "../index.js";
+function repository_lokal() {
+}
 /*  ZUM REPOSITORY ERSTELLEN:
     -->LOKAL
     restic init --repo /srv/restic-repo
@@ -14,6 +16,15 @@
     restic -r s3:s3.amazonaws.com/bucket_name init
     Passwort mit übergeben
     Passwort ein zweites Mal eingeben
+    
+    var initial = Repo{
+        Name: "Backup",
+        Type: "s3",
+        Location: "s3:s3.amazonaws.com/bucket_name",
+        EnvVariables: []string{"AWS_ACCESS_KEY_ID=MY_ACCESS_KEY", "AWS_SECRET_ACCESS_KEY=MY_SECRET_ACCESS_KEY"},
+        ID: 0, //ignored when id == 0
+        Password: "password",
+    }
 
 */ 
 //# sourceMappingURL=index.js.map
