554c0fd52d745b17a7f03bf397937c72aecbfb80
Deployer <backup380@example.com>
2019-01-17 09:00:01 +0000

    Updates

--- service/build/index.js
+++ service/build/index.js
@@ -11,7 +11,7 @@ const app = express();
 // allow every origin to access the api
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 app.use(express.json());
