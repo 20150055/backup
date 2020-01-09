4514531388bf793af798175ef28b33e8dcfd5a8d
Deployer <backup380@example.com>
2020-01-09 10:31:17 +0000

    Updates

--- scripts/generatecert.js
+++ scripts/generatecert.js
@@ -5,13 +5,13 @@ var pems = selfsigned.generate(attrs, { days: 10000 });
 console.log(pems)
 
 
-fs.writeFile("../service/cert.cert", pems.cert, function(err) {
+fs.writeFile("../service/build/cert.cert", pems.cert, function(err) {
     if(err) {
         return console.log(err);
     }
     console.log("The cert file was created");
 }); 
-fs.writeFile("../service/key.key", pems.cert, function(err) {
+fs.writeFile("../service/build/key.key", pems.cert, function(err) {
     if(err) {
         return console.log(err);
     }
